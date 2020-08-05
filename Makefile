d203167a9a6f970768b93ccb6471e997f996319e
FdogMain <2506897252@qq.com>
2020-08-05 14:14:25 +0800

    添加了背景

--- Makefile
+++ Makefile
@@ -210,6 +210,7 @@ Makefile: FdogMusicPlayer.pro ..\..\QT\5.10.0\msvc2015_64\mkspecs\win32-msvc\qma
 		..\..\QT\5.10.0\msvc2015_64\mkspecs\features\qt_config.prf \
 		..\..\QT\5.10.0\msvc2015_64\mkspecs\win32-msvc\qmake.conf \
 		..\..\QT\5.10.0\msvc2015_64\mkspecs\features\spec_post.prf \
+		.qmake.stash \
 		..\..\QT\5.10.0\msvc2015_64\mkspecs\features\exclusive_builds.prf \
 		..\..\QT\5.10.0\msvc2015_64\mkspecs\common\msvc-version.conf \
 		..\..\QT\5.10.0\msvc2015_64\mkspecs\features\toolchain.prf \
@@ -374,6 +375,7 @@ Makefile: FdogMusicPlayer.pro ..\..\QT\5.10.0\msvc2015_64\mkspecs\win32-msvc\qma
 ..\..\QT\5.10.0\msvc2015_64\mkspecs\features\qt_config.prf:
 ..\..\QT\5.10.0\msvc2015_64\mkspecs\win32-msvc\qmake.conf:
 ..\..\QT\5.10.0\msvc2015_64\mkspecs\features\spec_post.prf:
+.qmake.stash:
 ..\..\QT\5.10.0\msvc2015_64\mkspecs\features\exclusive_builds.prf:
 ..\..\QT\5.10.0\msvc2015_64\mkspecs\common\msvc-version.conf:
 ..\..\QT\5.10.0\msvc2015_64\mkspecs\features\toolchain.prf:
--- ui_mainwindow.h
+++ ui_mainwindow.h
@@ -13,11 +13,16 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QMenuBar>
-#include <QtWidgets/QStatusBar>
-#include <QtWidgets/QToolBar>
+#include <QtWidgets/QPlainTextEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSlider>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QToolButton>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -25,28 +30,181 @@ QT_BEGIN_NAMESPACE
 class Ui_MainWindow
 {
 public:
-    QMenuBar *menuBar;
-    QToolBar *mainToolBar;
     QWidget *centralWidget;
-    QStatusBar *statusBar;
+    QFrame *frame_3;
+    QFrame *frame;
+    QHBoxLayout *horizontalLayout;
+    QLabel *label_2;
+    QSlider *horizontalSlider;
+    QLabel *label;
+    QFrame *frame_2;
+    QHBoxLayout *horizontalLayout_3;
+    QSpacerItem *horizontalSpacer_4;
+    QPushButton *pushButton_1;
+    QSpacerItem *horizontalSpacer_2;
+    QPushButton *pushButton_2;
+    QSpacerItem *horizontalSpacer_3;
+    QPushButton *pushButton_3;
+    QSpacerItem *horizontalSpacer_5;
+    QFrame *frame_4;
+    QHBoxLayout *horizontalLayout_4;
+    QToolButton *toolButton_1;
+    QToolButton *toolButton_2;
+    QToolButton *toolButton_3;
+    QToolButton *toolButton_4;
+    QPlainTextEdit *plainTextEdit;
+    QPushButton *pushButton;
+    QLabel *label_3;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->resize(400, 300);
-        menuBar = new QMenuBar(MainWindow);
-        menuBar->setObjectName(QStringLiteral("menuBar"));
-        MainWindow->setMenuBar(menuBar);
-        mainToolBar = new QToolBar(MainWindow);
-        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
-        MainWindow->addToolBar(mainToolBar);
+        MainWindow->resize(413, 681);
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        frame_3 = new QFrame(centralWidget);
+        frame_3->setObjectName(QStringLiteral("frame_3"));
+        frame_3->setGeometry(QRect(10, 519, 391, 151));
+        frame_3->setMinimumSize(QSize(391, 151));
+        frame_3->setFrameShape(QFrame::StyledPanel);
+        frame_3->setFrameShadow(QFrame::Raised);
+        frame = new QFrame(frame_3);
+        frame->setObjectName(QStringLiteral("frame"));
+        frame->setGeometry(QRect(10, 10, 371, 42));
+        frame->setFrameShape(QFrame::StyledPanel);
+        frame->setFrameShadow(QFrame::Raised);
+        horizontalLayout = new QHBoxLayout(frame);
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        label_2 = new QLabel(frame);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setMinimumSize(QSize(30, 0));
+        label_2->setMaximumSize(QSize(30, 16777215));
+        label_2->setAlignment(Qt::AlignCenter);
+
+        horizontalLayout->addWidget(label_2);
+
+        horizontalSlider = new QSlider(frame);
+        horizontalSlider->setObjectName(QStringLiteral("horizontalSlider"));
+        horizontalSlider->setEnabled(true);
+        horizontalSlider->setMinimumSize(QSize(153, 0));
+        horizontalSlider->setOrientation(Qt::Horizontal);
+
+        horizontalLayout->addWidget(horizontalSlider);
+
+        label = new QLabel(frame);
+        label->setObjectName(QStringLiteral("label"));
+        label->setMinimumSize(QSize(30, 0));
+        label->setMaximumSize(QSize(30, 16777215));
+        label->setAlignment(Qt::AlignCenter);
+
+        horizontalLayout->addWidget(label);
+
+        frame_2 = new QFrame(frame_3);
+        frame_2->setObjectName(QStringLiteral("frame_2"));
+        frame_2->setGeometry(QRect(10, 50, 371, 91));
+        frame_2->setFrameShape(QFrame::StyledPanel);
+        frame_2->setFrameShadow(QFrame::Raised);
+        horizontalLayout_3 = new QHBoxLayout(frame_2);
+        horizontalLayout_3->setSpacing(6);
+        horizontalLayout_3->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout_3->setObjectName(QStringLiteral("horizontalLayout_3"));
+        horizontalSpacer_4 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_3->addItem(horizontalSpacer_4);
+
+        pushButton_1 = new QPushButton(frame_2);
+        pushButton_1->setObjectName(QStringLiteral("pushButton_1"));
+        pushButton_1->setMinimumSize(QSize(50, 50));
+        pushButton_1->setStyleSheet(QStringLiteral("border-image: url(:/lib/shangyiqu.png);"));
+
+        horizontalLayout_3->addWidget(pushButton_1);
+
+        horizontalSpacer_2 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_3->addItem(horizontalSpacer_2);
+
+        pushButton_2 = new QPushButton(frame_2);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+        pushButton_2->setMinimumSize(QSize(60, 60));
+        pushButton_2->setStyleSheet(QStringLiteral("border-image: url(:/lib/stop.png);"));
+
+        horizontalLayout_3->addWidget(pushButton_2);
+
+        horizontalSpacer_3 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_3->addItem(horizontalSpacer_3);
+
+        pushButton_3 = new QPushButton(frame_2);
+        pushButton_3->setObjectName(QStringLiteral("pushButton_3"));
+        pushButton_3->setMinimumSize(QSize(50, 50));
+        pushButton_3->setStyleSheet(QStringLiteral("border-image: url(:/lib/shangyiqu.png);"));
+
+        horizontalLayout_3->addWidget(pushButton_3);
+
+        horizontalSpacer_5 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_3->addItem(horizontalSpacer_5);
+
+        frame_4 = new QFrame(centralWidget);
+        frame_4->setObjectName(QStringLiteral("frame_4"));
+        frame_4->setGeometry(QRect(286, -6, 131, 31));
+        frame_4->setFrameShape(QFrame::StyledPanel);
+        frame_4->setFrameShadow(QFrame::Raised);
+        horizontalLayout_4 = new QHBoxLayout(frame_4);
+        horizontalLayout_4->setSpacing(6);
+        horizontalLayout_4->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout_4->setObjectName(QStringLiteral("horizontalLayout_4"));
+        toolButton_1 = new QToolButton(frame_4);
+        toolButton_1->setObjectName(QStringLiteral("toolButton_1"));
+        toolButton_1->setMinimumSize(QSize(0, 20));
+        toolButton_1->setStyleSheet(QStringLiteral("border-image: url(:/lib/xuanxiang.png);"));
+        toolButton_1->setPopupMode(QToolButton::DelayedPopup);
+        toolButton_1->setToolButtonStyle(Qt::ToolButtonIconOnly);
+        toolButton_1->setAutoRaise(false);
+        toolButton_1->setArrowType(Qt::NoArrow);
+
+        horizontalLayout_4->addWidget(toolButton_1);
+
+        toolButton_2 = new QToolButton(frame_4);
+        toolButton_2->setObjectName(QStringLiteral("toolButton_2"));
+        toolButton_2->setMinimumSize(QSize(0, 20));
+        toolButton_2->setStyleSheet(QStringLiteral("border-image: url(:/lib/zuixiao.png);"));
+
+        horizontalLayout_4->addWidget(toolButton_2);
+
+        toolButton_3 = new QToolButton(frame_4);
+        toolButton_3->setObjectName(QStringLiteral("toolButton_3"));
+        toolButton_3->setMinimumSize(QSize(0, 20));
+        toolButton_3->setStyleSheet(QStringLiteral("border-image: url(:/lib/zuidahua.png);"));
+
+        horizontalLayout_4->addWidget(toolButton_3);
+
+        toolButton_4 = new QToolButton(frame_4);
+        toolButton_4->setObjectName(QStringLiteral("toolButton_4"));
+        toolButton_4->setMinimumSize(QSize(0, 20));
+        toolButton_4->setStyleSheet(QString::fromUtf8("border-image: url(:/lib/\345\205\263  \351\227\255.png);"));
+
+        horizontalLayout_4->addWidget(toolButton_4);
+
+        plainTextEdit = new QPlainTextEdit(centralWidget);
+        plainTextEdit->setObjectName(QStringLiteral("plainTextEdit"));
+        plainTextEdit->setGeometry(QRect(33, 60, 341, 441));
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setGeometry(QRect(0, 30, 75, 23));
+        label_3 = new QLabel(centralWidget);
+        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setGeometry(QRect(0, 0, 421, 28));
+        label_3->setStyleSheet(QStringLiteral("background-color: rgba(170, 85, 255,100);"));
         MainWindow->setCentralWidget(centralWidget);
-        statusBar = new QStatusBar(MainWindow);
-        statusBar->setObjectName(QStringLiteral("statusBar"));
-        MainWindow->setStatusBar(statusBar);
+        label_3->raise();
+        frame_3->raise();
+        frame_4->raise();
+        plainTextEdit->raise();
+        pushButton->raise();
 
         retranslateUi(MainWindow);
 
@@ -56,6 +214,17 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        label_2->setText(QApplication::translate("MainWindow", "0:00", nullptr));
+        label->setText(QApplication::translate("MainWindow", "4:32", nullptr));
+        pushButton_1->setText(QString());
+        pushButton_2->setText(QString());
+        pushButton_3->setText(QString());
+        toolButton_1->setText(QString());
+        toolButton_2->setText(QString());
+        toolButton_3->setText(QString());
+        toolButton_4->setText(QString());
+        pushButton->setText(QApplication::translate("MainWindow", "\346\267\273\345\212\240\351\237\263\351\242\221", nullptr));
+        label_3->setText(QString());
     } // retranslateUi
 
 };
